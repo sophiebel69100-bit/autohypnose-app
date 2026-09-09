914da5c05cc6e87a900d2261802dc86a433852fd
SOPHIE BEL Neuro Thérapeute <sophiebel69100@gmail.com>
2026-09-09 14:14:12 +0200

    Update generate-audio-background.js

--- netlify/functions/generate-audio-background.js
+++ netlify/functions/generate-audio-background.js
@@ -31,7 +31,7 @@ exports.handler = async (event) => {
       body: JSON.stringify({
         text: scriptAvecPauses,
         model_id: 'eleven_multilingual_v2',
-        voice_settings: { stability: 0.6, similarity_boost: 0.8, speed: 0.8 }
+        voice_settings: { stability: 0.6, similarity_boost: 0.8, speed: 0.7 }
       })
     });
 
